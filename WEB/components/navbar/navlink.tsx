6b4e2a07776142ae719611bd8cd97257dd904266
Naufal Azmi Mukhlis <naufal_azmi_mukhlis@teknokrat.ac.id>
2025-11-22 10:55:34 +0700

    Deskripsi singkat perubahan lokal saya

--- WEB/components/navbar/navlink.tsx
+++ WEB/components/navbar/navlink.tsx
@@ -2,119 +2,118 @@
 
 import Link from "next/link";
 import { useState } from "react";
-import {IoClose, IoMenu} from "react-icons/io5";
-import clsx from "clsx";    
+import { IoClose, IoMenu } from "react-icons/io5";
+import clsx from "clsx";
 const Navlink = () => {
     const [open, setOpen] = useState(false);
     return (
         <>
-        <button onClick={() => setOpen(!open)} className='inline-flex items-center p-2 justify-center text-sm text-gray-500
+            <button onClick={() => setOpen(!open)} className='inline-flex items-center p-2 justify-center text-sm text-gray-500
         rounded-md md:hidden hover:bg-gray-100'>
-            {!open ? <IoMenu className="size-8" /> : <IoClose className="size-8" />}
-        </button>
-        <div className={clsx("w-full md:block md:w-auto", {
-            "hidden": !open
-        })}>
-            <ul className=
-            "flex flex-col font-semibold text-sm uppercase p-4 mt-4 rounded-sm
+                {!open ? <IoMenu className="size-8" /> : <IoClose className="size-8" />}
+            </button>
+            <div className={clsx("w-full md:block md:w-auto", {
+                "hidden": !open
+            })}>
+                <ul className="flex flex-col font-semibold text-sm uppercase p-4 mt-4 rounded-sm
             bg-gray-50 md:flex-row md:items-center md:space-x-10 md:p-0 md:mt-0 md:border-0
             md:bg-white">
-                <li>
-                    <Link href="/" 
-                    className="block py-2 px-3 text-gray-800 hover:bg-gray-100
+                    <li>
+                        <Link href="/"
+                            className="block py-2 px-3 text-gray-800 hover:bg-gray-100
                     rounded-sm md:hover:bg-transparent md:p-0"
-                    >
-                        Home
-                        
-                        
+                        >
+                            Home
+
+
                         </Link>
 
-                </li>
+                    </li>
 
-                <li>
-                    <Link href="/about" 
-                    className="block py-2 px-3 text-gray-800 hover:bg-gray-100
+                    <li>
+                        <Link href="/about"
+                            className="block py-2 px-3 text-gray-800 hover:bg-gray-100
                     rounded-sm md:hover:bg-transparent md:p-0"
-                    >
-                        About
-                        
-                        
+                        >
+                            About
+
+
                         </Link>
 
-                </li>
+                    </li>
 
-                <li>
-                    <Link href="/room" 
-                    className="block py-2 px-3 text-gray-800 hover:bg-gray-100
+                    <li>
+                        <Link href="/room"
+                            className="block py-2 px-3 text-gray-800 hover:bg-gray-100
                     rounded-sm md:hover:bg-transparent md:p-0"
-                    >
-                        Rooms
-                        
-                        
+                        >
+                            Rooms
+
+
                         </Link>
 
-                </li>
+                    </li>
 
-                <li>
-                    <Link href="/contact" 
-                    className="block py-2 px-3 text-gray-800 hover:bg-gray-100
+                    <li>
+                        <Link href="/contact"
+                            className="block py-2 px-3 text-gray-800 hover:bg-gray-100
                     rounded-sm md:hover:bg-transparent md:p-0"
-                    >
-                        Contact
-                        
-                        
+                        >
+                            Contact
+
+
                         </Link>
 
-                </li>
+                    </li>
 
-                                <li>
-                    <Link href="/myreservation" 
-                    className="block py-2 px-3 text-gray-800 hover:bg-gray-100
+                    <li>
+                        <Link href="/myreservation"
+                            className="block py-2 px-3 text-gray-800 hover:bg-gray-100
                     rounded-sm md:hover:bg-transparent md:p-0"
-                    >
-                        My Reservation 
-                        
-                        
+                        >
+                            My Reservation
+
+
                         </Link>
 
-                </li>
+                    </li>
 
-                                <li>
-                    <Link href="admin/dashboard" 
-                    className="block py-2 px-3 text-gray-800 hover:bg-gray-100
+                    <li>
+                        <Link href="admin/dashboard"
+                            className="block py-2 px-3 text-gray-800 hover:bg-gray-100
                     rounded-sm md:hover:bg-transparent md:p-0"
-                    >
-                        Dashboard
-                        
-                        
+                        >
+                            Dashboard
+
+
                         </Link>
 
-                </li>
+                    </li>
 
-                                <li>
-                    <Link href="admin/room" 
-                    className="block py-2 px-3 text-gray-800 hover:bg-gray-100
+                    <li>
+                        <Link href="admin/room"
+                            className="block py-2 px-3 text-gray-800 hover:bg-gray-100
                     rounded-sm md:hover:bg-transparent md:p-0"
-                    >
-                        Manage Room
-                        
-                        
+                        >
+                            Manage Room
+
+
                         </Link>
 
-                </li>
-                <li className="pt-2 md:pt-0">
-                    <Link href="/signin" className="py-2.5 px-6 bg-[#C2A895] text-white hover:bg-[#b39683]
+                    </li>
+                    <li className="pt-2 md:pt-0">
+                        <Link href="/signin" className="py-2.5 px-6 bg-[#C2A895] text-white hover:bg-[#b39683]
                     rounded-sm">
-                        Sign In
-                        
+                            Sign In
+
                         </Link>
 
-                </li>
+                    </li>
 
 
 
-            </ul>
-        </div>
+                </ul>
+            </div>
 
         </>
 
