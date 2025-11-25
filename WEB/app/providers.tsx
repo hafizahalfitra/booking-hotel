7d4622dc8fef7d17bb1255c1436c56904ef9e0f4
Naufal Azmi Mukhlis <naufal_azmi_mukhlis@teknokrat.ac.id>
2025-11-25 18:49:57 +0700

    membenarkan autentikasi

--- WEB/app/providers.tsx
+++ WEB/app/providers.tsx
@@ -1,1 +1,10 @@
 // prividers.tsx
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+import React from "react";
+
+export function NextAuthProviders({ children }: { children: React.ReactNode }) {
+  // SessionProvider harus membungkus semua komponen yang menggunakan NextAuth.js
+  return <SessionProvider>{children}</SessionProvider>;
+}
