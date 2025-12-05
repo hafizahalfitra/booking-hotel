92942af52b6efb47adfc83a1e5266e8a683fdd32
Naufal Azmi Mukhlis <naufal_azmi_mukhlis@teknokrat.ac.id>
2025-12-05 19:47:49 +0700

    memperbarui autentikasi

--- WEB/app/providers.tsx
+++ WEB/app/providers.tsx
@@ -1,7 +1,16 @@
+// web/app/providers.tsx
 'use client';
 
 import { SessionProvider } from "next-auth/react";
+import Navbar from "@/components/navbar/navbar"; // atau "@/components/navbar/navbar"
+import Footer from "@/components/footer"; // atau "@/components/footer/footer"
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <Navbar />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </SessionProvider>
+  );
 }
