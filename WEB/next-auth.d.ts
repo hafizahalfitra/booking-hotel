1626019ca7885e15756c786e0b5043ce2a5a2819
Naufal Azmi Mukhlis <naufal_azmi_mukhlis@teknokrat.ac.id>
2025-12-05 19:50:47 +0700

    memperbarui autentikasi

---
+++ WEB/next-auth.d.ts
@@ -0,0 +1,13 @@
+// web/next-auth.d.ts
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id?: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    }
+  }
+}
