36ba961e3828dccc34e7f20da0d0ed037b9ccad8
Naufal Azmi Mukhlis <naufal_azmi_mukhlis@teknokrat.ac.id>
2025-12-05 19:21:28 +0700

    memperbarui autentikasi

--- WEB/next.config.ts
+++ WEB/next.config.ts
@@ -15,3 +15,14 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3001/api/:path*', // Proxy to Backend
+      },
+    ];
+  },
+};
