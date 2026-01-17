9f1542a78bb181d4c5f9fcd87b58d543bd5cf95e
apriliyaananta <apriliya_ananta@teknokrat.ac.id>
2026-01-17 11:57:32 +0700

    Update page.tsx

--- api/app/page.tsx
+++ api/app/page.tsx
@@ -49,6 +49,8 @@ export default function Home() {
             center.
           </p>
         </div>
+
+        // Bagian Tombol Action (Deploy & Docs)
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
