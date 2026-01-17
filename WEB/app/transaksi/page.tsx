31ce88bcb71c2ad32dca44e360ede53aba0a666c
apriliyaananta <apriliya_ananta@teknokrat.ac.id>
2026-01-17 12:14:52 +0700

    Update page.tsx

--- WEB/app/transaksi/page.tsx
+++ WEB/app/transaksi/page.tsx
@@ -32,7 +32,7 @@ export default function MyReservationPage() {
     // State Management
     // transactions: Array untuk menyimpan daftar riwayat pesanan
     const [transactions, setTransactions] = useState<Transaction[]>([]);
-    // transactions: Array untuk menyimpan daftar riwayat pesanan
+    // loading: Indikator untuk menampilkan spinner saat data sedang diambil
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
