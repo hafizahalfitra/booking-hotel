4bb425773e4e6efeb63b41fdafe48970a0c1ad7c
apriliyaananta <apriliya_ananta@teknokrat.ac.id>
2026-01-17 11:52:33 +0700

    Update page.tsx

--- api/app/page.tsx
+++ api/app/page.tsx
@@ -9,6 +9,8 @@ export default function Home() {
 
       // Main Content Wrapper:
       // - Membatasi lebar maksimal (max-w-3xl) agar konten tidak terlalu lebar di layar besar.
+      // - Pada layar kecil (mobile), item disusun vertikal (flex-col items-center).
+      
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
