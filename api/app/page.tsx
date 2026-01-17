b65d010c067f14141fab561a90d999dcb2d36520
apriliyaananta <apriliya_ananta@teknokrat.ac.id>
2026-01-17 11:52:00 +0700

    Update page.tsx

--- api/app/page.tsx
+++ api/app/page.tsx
@@ -7,7 +7,8 @@ export default function Home() {
     // Mendukung Dark Mode (dark:bg-black)
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
 
-      
+      // Main Content Wrapper:
+      // - Membatasi lebar maksimal (max-w-3xl) agar konten tidak terlalu lebar di layar besar.
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
